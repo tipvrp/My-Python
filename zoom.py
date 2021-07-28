d3959ea5b5bc0f2d1907438643d433ff6ee11a46
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-07-28 21:02:55 +0700

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -2,15 +2,15 @@ import webbrowser
 import datetime
 dt_now = datetime.datetime.now() # M0 T1 W2 H3 F4 S5 Sun6 
 n=10
-url = [ "https://zoom.us/j/92566534389?pwd=Y2FSbkhwOTZUS2xLai96NTJ3NmJVUT09", #English Conversation M9-12   0
-        "https://zoom.us/j/7505560466?pwd=SzZFblRleVVsaVE2L0YweGgwNDBVUT09", #Select topic M13-16           1
-        "https://zoom.us/j/8818507908?pwd=b3cvckxjMDNLVzhWeWY5a3dRRGZwdz09", #Optical M17-20                2
-        "https://zoom.us/j/6693797485?pwd=cWVFc3BMVVZsNFVucWhSaVpkS1lIQT09", #Machine Learning T13-16       3
-        "https://zoom.us/j/6456457914?pwd=MEN1SjJMaUwxZVR0Q242L042K2dmZz09", #Compro TA W13-15              4
-        "https://zoom.us/j/97131084116?pwd=R1AvdXJBc0Vsa29keFd5cFYvdGVIZz09", #Bridge H10-12                5
-        "https://zoom.us/j/7281157598?pwd=cDllSm81QkdMc1FaR0c5S2V2U2dxUT09", #Computer Network H13-16       6
-        "https://zoom.us/j/6456457914?pwd=MEN1SjJMaUwxZVR0Q242L042K2dmZz09", #Compro TA F PIV               7
-        "https://zoom.us/j/8671901631?pwd=YzJ5MzlxMTE3TVM5SHRBTGw0YklEUT09", #Compro TA F RSK               8
+url = [ "https://zoom.us/j/***********************************************", #English Conversation M9-12   0
+        "https://zoom.us/j/***********************************************", #Select topic M13-16          1
+        "https://zoom.us/j/***********************************************", #Optical M17-20               2
+        "https://zoom.us/j/***********************************************", #Machine Learning T13-16      3
+        "https://zoom.us/j/***********************************************", #Compro TA W13-15             4
+        "https://zoom.us/j/***********************************************", #Bridge H10-12                5
+        "https://zoom.us/j/***********************************************", #Computer Network H13-16      6
+        "https://zoom.us/j/***********************************************", #Compro TA F PIV              7
+        "https://zoom.us/j/***********************************************", #Compro TA F RSK              8
       ]
 #-------------------------------------------------------------- Monday
 if dt_now.weekday() == 0 and 8 <= dt_now.hour < 12 :
