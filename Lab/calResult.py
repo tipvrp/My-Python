32667c39168e98283cfb61c0ba40dfd6fd4250c7
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-05 01:23:50 +0700

    Create calResult.py

---
+++ Lab/calResult.py
@@ -0,0 +1,8 @@
+print('calResult')
+a = float(input('Please Enter a:'))
+b = float(input('Please Enter b:'))
+c = float(input('Please Enter c:'))
+
+Result = ((4.2*a)+(2.8*b))/(((5*b)/a)-(7*c))
+
+print('Result is '+str(Result))
